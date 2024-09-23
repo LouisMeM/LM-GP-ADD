9d1049b182b7d496e1bc171774df0af080655fb5
Alexandre_DD <alexandre.delaval.dupuis@gmail.com>
2024-09-23 17:46:20 +0200

    - ajout de commentaires dans library.cs

--- Delaval-Dupuis_Perrier_Mamtchourouk/Library.cs
+++ Delaval-Dupuis_Perrier_Mamtchourouk/Library.cs
@@ -5,28 +5,39 @@ using System.Net.Mime;
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Librabry
+public class Library
 {
 	// Declare an array of Media objects
 	private List<Media> biblioteque = new List<Media>();
 
-
+	/// <summary>
+	/// Ajouter un media (via sa référence : int) à la Bibliotheque (list)
+	/// </summary>
+	/// <param name="ref"></param>
 	public void ajouterMedia(int ref)
 	{
         biblioteque.Add(ref);
     }
 
-	public void supprimerMedia(int ref)
+    /// <summary>
+    /// Supprimer un media (via sa référence : int) de la Bibliotheque (list)
+    /// </summary>
+    /// <param name="ref"></param>
+    public void supprimerMedia(int ref)
 	{		
         biblioteque.Remove(ref)
 	}
 
-	public int emprunterMedia(int ref)
+    /// <summary>
+    /// "Emprunter" un media (via sa référence : int) à la Bibliotheque (list)
+    /// </summary>
+    /// <param name="ref"></param>
+    public int emprunterMedia(int ref)
 	{
 		mediaEmprunte = biblioteque.Remove(ref)
 	}
 
-	public Librabry()
+	public Library()
 	{
 		//
 		// TODO: Add constructor logic here
